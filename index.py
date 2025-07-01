32736e2f9205f2d243f441ceb83606243bd4de4f
Humphrey Ngugi <125015338+humphrey1000@users.noreply.github.com>
2025-07-01 16:32:00 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 import sys
-def parser():
-	""" \
-            Parsing data
- 	    Data types: variables, functions, data structures, operators, classes
- 	"""
-		
 
-	
-	if __name__ == '__main__':
-		prose = sys.argv[1]
-	    	parser(prose)
-		
+
+def parser(prose):
+    """ \
+            Parsing data
+        Data types: variables, functions, data structures, operators, classes
+    """
+
+    if __name__ == '__main__':
+        prose = sys.argv[1]
+        parser(prose)

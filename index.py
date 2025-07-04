dd30eee1b7ee9a95dd3ef2e9bcfc9e93acc24156
Humphrey Ngugi <125015338+humphrey1000@users.noreply.github.com>
2025-07-04 18:16:12 +0300

    Update index.py

--- index.py
+++ index.py
@@ -9,9 +9,10 @@ def parser(regexp, prose):
     text = re.findall(regexp, prose)
     print(text)
 
+
+# if __name__ == '__main__':
+#     text = sys.argv[1], sys.argv[2]
+#     parser(*text)
+
 well = "hello", "howdy means how do ye, anyway hello"
 parser(*well)
-
-    # if __name__ == '__main__':
-    #     text = sys.argv[1], sys.argv[2]
-    #     parser(*text)
